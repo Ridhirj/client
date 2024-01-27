e0b842f4b3d1aa626f67af2b2f1ad53fa78e9fa8
Hero <76834176+jish0101@users.noreply.github.com>
2024-01-27 22:18:06 +0530

    es-lint and prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-/** @format */
-
-import {
-  Button,
-  Group,
-  Loader,
-  Paper,
-  Text,
-  useMantineColorScheme,
-} from "@mantine/core";
+// import {
+//   Button,
+//   Group,
+//   Loader,
+//   Paper,
+//   Text,
+//   useMantineColorScheme,
+// } from "@mantine/core";
 
 function App() {
-  const { setColorScheme, clearColorScheme } = useMantineColorScheme({
-    keepTransitions: true,
-  });
+  // const { setColorScheme, clearColorScheme } = useMantineColorScheme({
+  //   keepTransitions: true,
+  // });
 
   return (
     <>
-      <Paper>
-        <Text>Whereas recognition of the inherent dignity</Text>
-        <Button>Lorem, ipsum dolor.</Button>
-        <Loader />
-      </Paper>
-
-      <Group>
+      Hello, This needs to be a very great and functional app.
+      {/* <Group>
         <Button onClick={() => setColorScheme("light")}>Light</Button>
         <Button onClick={() => setColorScheme("dark")}>Dark</Button>
         <Button onClick={() => setColorScheme("auto")}>Auto</Button>
         <Button onClick={clearColorScheme}>Clear</Button>
-      </Group>
+      </Group> */}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { MantineProvider } from "@mantine/core";
-import "@mantine/core/styles.css";
-import "./index.css";
-import { colorSchemeManager, theme } from "./theme/themeConfig.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import './index.css';
+import { colorSchemeManager, theme } from './Theme/themeConfig.js';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <MantineProvider
       defaultColorScheme="dark"
       theme={theme}
-      colorSchemeManager={colorSchemeManager}
-    >
+      colorSchemeManager={colorSchemeManager}>
       <App />
     </MantineProvider>
   </React.StrictMode>
