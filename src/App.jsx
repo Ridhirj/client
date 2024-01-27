96d1152dbcc099281a4d2b98dcf860e7fbef066b
Hero <76834176+jish0101@users.noreply.github.com>
2024-01-27 21:51:27 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/** @format */
+
+import {
+  Button,
+  Group,
+  Loader,
+  Paper,
+  Text,
+  useMantineColorScheme,
+} from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { setColorScheme, clearColorScheme } = useMantineColorScheme({
+    keepTransitions: true,
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Paper>
+        <Text>Whereas recognition of the inherent dignity</Text>
+        <Button>Lorem, ipsum dolor.</Button>
+        <Loader />
+      </Paper>
+
+      <Group>
+        <Button onClick={() => setColorScheme("light")}>Light</Button>
+        <Button onClick={() => setColorScheme("dark")}>Dark</Button>
+        <Button onClick={() => setColorScheme("auto")}>Auto</Button>
+        <Button onClick={clearColorScheme}>Clear</Button>
+      </Group>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "./index.css";
+import { colorSchemeManager, theme } from "./theme/themeConfig.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider
+      defaultColorScheme="dark"
+      theme={theme}
+      colorSchemeManager={colorSchemeManager}
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
